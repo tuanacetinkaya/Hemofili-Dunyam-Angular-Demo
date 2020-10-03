57795f00d35ad82625f30d5902865eca6361bb70
Tuana <tuanacetinkaya7@gmail.com>
2020-10-03 16:16:37 +0300

    smooth scroll working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+// import { ExplainingComponent } from './components/explaining/explaining.component';
+// import { MyWorldComponent } from './components/my-world/my-world.component';
+// import { LivingComponent } from './components/living/living.component';
+// import { SupportComponent } from './components/support/support.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
+  // { path: '/hemofiliyi-anlamak', component: ExplainingComponent },
+  // { path: '/hemofili-dunyam', component: MyWorldComponent },
+  // { path: '/hemofili-dunyasinda-yasamak', component: LivingComponent },
+  // { path: '/budunyanindestekcileri', component: SupportComponent },
+
 ];
 
 @NgModule({
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -11,5 +11,8 @@ export class NavBarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  sidebarCollapse(){
+    
+  }
 
 }
